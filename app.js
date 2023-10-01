00a59b152f027c20db88add342536704fa15413e
nunion1 <theo.vandermarck@outlook.com>
2023-09-30 20:17:10 -0700

    2nd to last Co-authored-by: jacob-s-codes <jacob-s-codes@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var myData;
 var dataRead = false;
+var mainPage = false
+savedPeople = JSON.parse(localStorage.getItem("Saved People"))
+// if (savedPeople==null){
+    localSavedPeople = []
+// }
+tmp=[]
+savedPeople = savedPeople.filter(function (v) {
+    if (tmp.indexOf(v.toString()) < 0) {
+        tmp.push(v.toString());
+        return v;
+    }
+});
+console.log("SP",savedPeople)
+// doFetch()
+// function doFetch(){
+console.log("started")
+
+if (window.location.pathname=="/index.html"){
+    console.log("yay")
+    mainPage = true;
+}
+
+if (mainPage){
 fetch('https://navy-scandalous-tilapia.glitch.me/api/profiles')
     .then(response => {
         if (!response.ok) {
@@ -18,6 +41,7 @@ fetch('https://navy-scandalous-tilapia.glitch.me/api/profiles')
         console.log('Fetch error:', error);
     });
 console.log("test")
+}
   
 // async function addData(name, description, classes, imageLink, phoneNumber, email){
 // try {
@@ -39,19 +63,43 @@ console.log("test")
 // }
 // }
 // name, description, classes, imageLink, phoneNumber, email
-async function addData(){ 
+async function addData(){
     let name = document.getElementById("userName").value
+    if (name!=""){
     let school = document.getElementById("school").value
+    let grade = document.getElementById("grade").value
     let description = document.getElementById("description").value
     let classes = []
     if (document.getElementById("englishBox").checked){
         classes.push("English")
     }
+    if (document.getElementById("geometeryBox").checked){
+        classes.push("Algebra 2")
+    }
+    
+    if (document.getElementById("algebraBox").checked){
+        classes.push("Geometry")
+    }
+    if (document.getElementById("precalculusBox").checked){
+        classes.push("PreCalculus")
+    }
+    if (document.getElementById("calculusBox").checked){
+        classes.push("Calculus")
+    }
+    if (document.getElementById("statsBox").checked){
+        classes.push("Stats")
+    }
+    if (document.getElementById("biologyBox").checked){
+        classes.push("Biology")
+    }
+    if (document.getElementById("chemistryBox").checked){
+        classes.push("Chemistry")
+    }
+    if (document.getElementById("physicsBox").checked){
+        classes.push("Physics")
+    }
     if (document.getElementById("historyBox").checked){
         classes.push("History")
-    }
-    if (document.getElementById("mathBox").checked){
-        classes.push("Math")
     }
     if (document.getElementById("spanishBox").checked){
         classes.push("Spanish")
@@ -71,7 +119,7 @@ async function addData(){
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({name, school, description, classes, imageLink, phoneNumber, email})
+            body: JSON.stringify({name, school, grade, description, classes, imageLink, phoneNumber, email})
         });
         const data = await response.json();
         if(response.status === 200) {
@@ -82,19 +130,48 @@ async function addData(){
     } catch (error) {
         console.error('Network error:', error);
     }
+    document.getElementById("userName").value = ""
+    // document.getElementById("grade").value = ""
+    document.getElementById("school").value = ""
+    document.getElementById("description").value = ""
+    document.getElementById("imageLink").value = ""
+    document.getElementById("phoneNumber").value = ""
+    document.getElementById("englishBox").checked = false
+    document.getElementById("geometeryBox").checked = false
+    document.getElementById("algebraBox").checked = false
+    document.getElementById("precalculusBox").checked = false
+    document.getElementById("calculusBox").checked = false
+    document.getElementById("statsBox").checked = false
+    document.getElementById("biologyBox").checked = false
+    document.getElementById("chemistryBox").checked = false
+    document.getElementById("physicsBox").checked = false
+    document.getElementById("historyBox").checked = false
+    document.getElementById("spanishBox").checked = false
+    document.getElementById("chineseBox").checked = false
+    document.getElementById("frenchBox").checked = false
+}else{
+    console.log("stupid lol")
+}
     }
 function myTest(){
     console.log("MYTEST")
 }
-let savedPeople = []
+// let savedPeople = []
 // let currentlyOn;
 let currentlyOn=0;
+
 async function loadFirst(){
     currentlyOn = 0;
     if (dataRead){
         let mdco = myData[currentlyOn]
         document.getElementById("nameEnd").innerHTML=mdco["name"]
+        // var val = document.getElementById('imagename').value,
+        mySrc = mdco["imageLink"]
+
+        document.getElementById("imageEnd").src = mySrc;
+        // document.getElementById("imageEnd").appendChild(img);
         document.getElementById("schoolEnd").innerHTML=mdco["school"]
+        document.getElementById("gradeEnd").innerHTML=mdco["grade"]
         document.getElementById("descriptionEnd").innerHTML=mdco["description"]
         let classesText = ""
         console.log(mdco["classes"].length)
@@ -116,32 +193,87 @@ async function loadFirst(){
         // }
     }
 }
-async function loadNext(){
+async function loadNext(direction){
     currentlyOn++
     
-    if (dataRead){
-        let mdco = myData[currentlyOn]
-        document.getElementById("nameEnd").innerHTML=mdco["name"]
-        document.getElementById("schoolEnd").innerHTML=mdco["school"]
-        document.getElementById("descriptionEnd").innerHTML=mdco["description"]
-        let classesText = ""
-        console.log(mdco["classes"].length)
-        for (let i = 0; i < mdco["classes"].length; i++){
-
-            if (i!=0){
-                classesText+=", "
-            }else{
-                classesText+="I can tutor: "
+    document.getElementById("personbox").classList.add("animateCard"+direction)
+    setTimeout(()=>{
+        document.getElementById("personbox").classList.remove("animateCard"+direction);
+        console.log(dataRead)
+        if (dataRead){
+            let mdco = myData[currentlyOn]
+            document.getElementById("nameEnd").innerHTML=mdco["name"]
+    
+            // console.log(mdco["imageLink"])
+            mySrc = mdco["imageLink"]
+    
+            document.getElementById("imageEnd").src = mySrc;
+            document.getElementById("schoolEnd").innerHTML=mdco["school"]
+            document.getElementById("gradeEnd").innerHTML=mdco["grade"]
+            document.getElementById("descriptionEnd").innerHTML=mdco["description"]
+            let classesText = ""
+            console.log(mdco["classes"].length)
+            for (let i = 0; i < mdco["classes"].length; i++){
+    
+                if (i!=0){
+                    classesText+=", "
+                }else{
+                    classesText+="I can tutor: "
+                }
+                classesText+=mdco["classes"][i]
             }
-            classesText+=mdco["classes"][i]
+            document.getElementById("classesEnd").innerHTML=classesText
         }
-        document.getElementById("classesEnd").innerHTML=classesText
-    }
+    
+ } , 300);
+    
+   
 }
 
 function saveCurrent(){
     let mdco = myData[currentlyOn]
-    savedPeople.push([mdco["name"], mdco["phoneNumber"], mdco["email"]]);
-    console.log(savedPeople)
-    loadNext()
+    localSavedPeople.push([mdco["name"], mdco["phoneNumber"], mdco["email"]]);
+    console.log()
+    localStorage.setItem("Saved People", JSON.stringify(localSavedPeople))
+    loadNext("Right")
+}
+
+function loadMyMatches(){
+    targetBox = document.getElementById("MyMatchesBox")
+    myDiv = document.getElementById("containerDiv")
+    console.log("b4",savedPeople)
+    savedPeople=removeDuplicates(savedPeople)
+    var tmp = [];
+
+// savedPeople = savedPeople.filter(function (v) {
+//     if (tmp.indexOf(v.toString()) < 0) {
+//         tmp.push(v.toString());
+//         return v;
+//     }
+// });
+    // console.log("after",b)
+    outputText = ""
+
+    
+      
+      // Select the table's tbody
+      const tbody = document.querySelector('#contactTable tbody');
+      
+      // Loop through the data array and add each data item as a row
+      savedPeople.forEach(item => {
+        const tr = document.createElement('tr');
+        
+        item.forEach(cellData => {
+          const td = document.createElement('td');
+          td.textContent = cellData;
+          tr.appendChild(td);
+        });
+        
+        tbody.appendChild(tr);
+      });
+}
+
+function removeDuplicates(arr) {
+    return arr.filter((item,
+        index) => arr.indexOf(item) === index);
 }
